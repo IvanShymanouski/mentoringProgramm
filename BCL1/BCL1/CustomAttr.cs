363beda66c9f80d9a545f836d08f753801151fc1
Ivan Shymanouski <shim_ivan@mail.ru>
2016-09-14 15:30:54 +0300

    файлы "BCL1" восстановлены

---
+++ BCL1/BCL1/CustomAttr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCL1
+{
+    [AttributeUsageAttribute(AttributeTargets.Class,  AllowMultiple = false)]
+    public class CustomAttr: Attribute
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public CustomAttr(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
---
+++ BCL1/BCL1/SomeClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCL1
+{
+    [CustomAttr("Ivan", "Ivan_Shymanouski@email.by")]
+    public class SomeClass: Attribute
+    {
+       public void print()
+       {
+           var type = this.GetType();
+           if (Attribute.IsDefined(type, typeof(CustomAttr))) // проверка на существование атрибута
+           {
+               var attributeValue = Attribute.GetCustomAttribute(type, typeof(CustomAttr)) as CustomAttr; // получаем значение атрибута
+               Console.WriteLine("Name : {0}, Email : {1}", attributeValue.Name, attributeValue.Email);
+           }
+       }
+    }
+}
