26d0d6037994311ad0ca9cbed579780de363c580
Ivan Shymanouski <shim_ivan@mail.ru>
2016-09-14 15:31:45 +0300

    файлы "Exceptions handling 2" восстановлены

---
+++ Exceptions_handling_2/CustomParsers/CustomParsers.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CustomParsers
+{
+    public static class IntParsers
+    {
+        public static int ParseCustom(this string src)
+        {
+            long result = 0,
+                 length;
+            bool sign = true;
+
+            if (src == null) throw new ArgumentNullException();
+            if (src.Length == 0) throw new FormatException("String is empty");
+
+            src = src.Trim();
+            length = src.Length;
+
+            if (length == 0) throw new FormatException("String contains only white spaces");
+
+            var i = 0;
+            switch (src[i])
+            {
+                case '-':
+                    sign = false;
+                    i++;
+                    break;
+                case '+':
+                    i++;
+                    break;
+            }
+
+            for (; i < length; i++)
+            {
+                if (char.IsDigit(src[i]))
+                {
+                    result = result * 10 + (src[i] - '0');
+                    if (sign)
+                    {
+                        if (result > int.MaxValue) throw new OverflowException("Tried to parse value greater than int.MaxValue");
+                    }
+                    else
+                    {
+                        if (-result < int.MinValue) throw new OverflowException("Tried to parse value less than int.MinValue");
+                    }
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+
+            return (int)((sign) ? result : -result);
+        }
+
+        public static bool TryParseCustom(this string src, out int result)
+        {
+            bool parsed = true;
+            try
+            {
+                result = src.ParseCustom();
+            }
+            catch (ArgumentNullException)
+            {
+                result = 0;
+                parsed = false;
+            }
+            catch (FormatException)
+            {
+                result = 0;
+                parsed = false;
+            }
+            catch(OverflowException)
+            {
+                result = 0;
+                parsed = false;
+            }
+
+            return parsed;
+        }
+    }
+}
---
+++ Exceptions_handling_2/Exceptions_handling_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using CustomParsers;
+using System;
+
+namespace Exceptions_handling_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] values = { null,  "+13230", "-0", "1,390,146", "$190,235,421,127",
+                          "0xFA1B", "163042", "-10", "2147483647", 
+                          "2147483648", "16e07", "134985.0", "-12034",
+                          "-2147483648", "-2147483649" };
+            foreach (string value in values)
+            {
+                try
+                {
+                    int number = value.ParseCustom();
+                    Console.WriteLine("{0} --> {1}", value, number);
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Attempted conversion of '<null>' failed.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0}: Bad Format", value);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("{0}: Overflow", value);
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("======  TryParse  =====");
+            Console.WriteLine(Environment.NewLine);
+
+            values = new string[] {null,  "160519", "9432.0", "16,667", "   -322   ", "+4302", "(100);", "01FA" };
+            foreach (var value in values)
+            {
+                int number;
+
+                bool result = value.TryParseCustom(out number);
+                if (result)
+                {
+                    Console.WriteLine("Converted '{0}' to {1}.", value, number);
+                }
+                else
+                {
+                    //            if (value == null) value = ""; 
+                    Console.WriteLine("Attempted conversion of '{0}' failed.",
+                                       value == null ? "<null>" : value);
+                } 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
