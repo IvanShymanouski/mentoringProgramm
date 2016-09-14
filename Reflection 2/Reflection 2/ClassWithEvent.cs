30165e15ba4e64b0da81679a44af423716706d7e
Ivan Shymanouski <shim_ivan@mail.ru>
2016-09-14 15:32:36 +0300

    файлы "Reflection 2" восстановлены

---
+++ Reflection 2/Reflection 2/ClassWithEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_2
+{
+    public class ClassWithInheritedEvent : ClassWithEventAndTrigger
+    {
+        public void amazingTrigger()
+        {
+            parentTrigger();
+        }
+    }
+
+    public class ClassWithEventAndTrigger
+    {
+        public delegate void EventMethodTemplate();
+
+        public event EventMethodTemplate onSomeEvent;
+
+        public void parentTrigger()
+        {
+            if (onSomeEvent != null)
+            {
+                onSomeEvent();
+            }
+        }
+    }
+}
---
+++ Reflection 2/Reflection 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_2
+{
+    class Program
+    {
+        public void Message()
+        {
+            Console.WriteLine("Hi, I'm instanse handler!");
+        }
+
+        public static void SecondMessage()
+        {
+            Console.WriteLine("Hi, I'm static handler!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Befor reflection---");
+
+            ClassWithInheritedEvent InsOfClass = Activator.CreateInstance<ClassWithInheritedEvent>();            
+            EventInfo eventInfo = InsOfClass.GetType().GetEvent("onSomeEvent");
+            Type handlerType = eventInfo.EventHandlerType;
+
+            //add instanse method
+            Program pInst = Activator.CreateInstance<Program>();
+            MethodInfo handler = pInst.GetType().GetMethod("Message", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance);
+            Delegate handlerDelegate = Delegate.CreateDelegate(handlerType, pInst, handler);
+            eventInfo.AddEventHandler(InsOfClass, handlerDelegate);
+
+            //add static method
+            handlerDelegate = Delegate.CreateDelegate(handlerType, typeof(Program), "SecondMessage");
+            eventInfo.AddEventHandler(InsOfClass, handlerDelegate);
+
+
+            MethodInfo triggerMethod = InsOfClass.GetType().GetMethod("amazingTrigger", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.DeclaredOnly); //null for parrentTrigger obviously
+
+            triggerMethod.Invoke(InsOfClass, null);
+
+            Console.WriteLine("---After trigger---");
+
+            Console.ReadKey();
+        }
+    }
+}
